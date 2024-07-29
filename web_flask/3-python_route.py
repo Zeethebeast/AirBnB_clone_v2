8d9fcb84ddf7894b8e843dd8fd1160b114e965d8
zeethebeast <chimasamuel6@gmail.com>
2024-07-29 05:48:47 +0100

    web_framework

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -18,7 +18,7 @@ def c_text(text):
     text = text.replace("_", " ")
     return "C {}".format(python_text)
 
-@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text):
     text = text.replace("_", " ")
